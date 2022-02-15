077d4b9a4a7747b655b49cc8ab8b0c109331017c
Blackoutburst <blackoutburst@hotmail.com>
2022-02-15 13:47:08 +0100

    Added basic config file

--- src/com/blackoutburst/quake/main/Main.java
+++ src/com/blackoutburst/quake/main/Main.java
@@ -11,9 +11,11 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +26,9 @@ public class Main extends JavaPlugin {
 	public static boolean gameRunning = false;
 	public static int gameTime = 0;
 	public static List<Location> respawns = new ArrayList<>();
-	
+	public static boolean allowMapSelector;
+	public static Location spawn;
+
 	public static World gameWorld;
 
 	@Override
@@ -33,10 +37,42 @@ public class Main extends JavaPlugin {
 		new Core().cooldownTimer();
 		new Core().gameTimer();
 		new File("./plugins/Quake/player data/").mkdirs();
+		new File("./plugins/Quake/config/").mkdirs();
 		SkullLoader.load();
 		Utils.spawnParticlesScheduler();
 
-		gameWorld = Bukkit.getWorld("world");
+		gameWorld = Bukkit.getWorlds().get(0);
+
+		File f = new File("plugins/Quake/config/config.yml");
+		YamlConfiguration file = YamlConfiguration.loadConfiguration(f);
+		if (!f.exists()) {
+			allowMapSelector = false;
+			spawn = new Location(Bukkit.getWorlds().get(0), 0.5, 5, 0.5, 0, 0);
+
+			file.set("allow-map-selector", false);
+			file.set("spawn", null);
+			file.set("spawn.world", Bukkit.getWorlds().get(0).getName());
+			file.set("spawn.x", 0.5);
+			file.set("spawn.y", 5);
+			file.set("spawn.z", 0.5);
+			file.set("spawn.yaw", 0);
+			file.set("spawn.pitch", 0);
+
+			try {
+				file.save(f);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			allowMapSelector = file.getBoolean("allow-map-selector");
+			World world = Bukkit.getWorld(file.getString("spawn.world"));
+			double x = file.getDouble("spawn.x");
+			double y = file.getDouble("spawn.y");
+			double z = file.getDouble("spawn.z");
+			float yaw = (float) file.getDouble("spawn.yaw");
+			float pitch = (float) file.getDouble("spawn.pitch");
+			spawn = new Location(world, x, y, z, yaw, pitch);
+		}
 	}
 	
 	@Override
